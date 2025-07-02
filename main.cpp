2904c35adaad92f01d729843e21ebf199fafc6c6
Milan Radosavljevic <milan.radosavljevic@amd.com>
2025-07-02 05:22:08 -0400

    implementation with spinlock

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <array>
 #include <atomic>
+#include <cassert>
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
+#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -14,6 +16,7 @@
 #include <string>
 #include <thread>
 #include <type_traits>
+#include <condition_variable>
 #include "benchmark.hpp"
 
 struct track_sample {
@@ -39,14 +42,52 @@ struct process_sample {
 };
 
 namespace cache {
+class spin_lock {
+private:
+  std::atomic_flag _lockFlag;
+public:
+  spin_lock() : _lockFlag{ATOMIC_FLAG_INIT} {}
+  spin_lock(const spin_lock&) = delete;
+  spin_lock& operator=(const spin_lock&) = delete;
+  spin_lock(const spin_lock&&) = delete;
+  spin_lock& operator=(const spin_lock&&) = delete;
+
+  void lock() {
+    while (_lockFlag.test_and_set(std::memory_order_acquire)) {
+    }
+  }
+
+  bool try_lock() { return !_lockFlag.test_and_set(std::memory_order_acquire); }
+
+  void unlock() { _lockFlag.clear(); }
+};
+
+class spin_lock_scope
+{
+private:
+  spin_lock& m_lock;
+public:
+  spin_lock_scope(spin_lock& spin_lock) : m_lock(spin_lock)
+  {
+    m_lock.lock();
+  }
+  ~spin_lock_scope()
+  {
+    m_lock.unlock();
+  }
+  spin_lock_scope(const spin_lock_scope&) = delete;
+  spin_lock_scope& operator=(const spin_lock_scope&) = delete;
+  spin_lock_scope(const spin_lock_scope&&) = delete;
+  spin_lock_scope& operator=(const spin_lock_scope&&) = delete;
+};
 
 constexpr auto KByte = 1024;
 constexpr auto MByte = 1024 * 1024;
-constexpr auto buffer_size = 10 * KByte;
-// constexpr auto buffer_size = 150;
+constexpr auto buffer_size = 10 * MByte;
+constexpr auto flush_treshhold = 5 * MByte;
 constexpr auto filename = "buffered_storage.bin";
 
-using BufferArray = std::array<uint8_t, buffer_size>;
+using buffer_array = std::array<uint8_t, buffer_size>;
 
 enum class sample_type : uint32_t { track = 1, process, fragmented_space = 0xFFFF };
 
@@ -58,6 +99,7 @@ public:
       : m_flushing_thread(std::thread([this]() {
           std::filesystem::path path{filename};
           std::ofstream ofs(path, std::ios::binary | std::ios::out);
+          std::mutex flushing_mutex;
 
           if (!ofs) {
             std::cerr << "Error opening file for writing: " << path
@@ -66,44 +108,38 @@ public:
           }
 
           auto execute_flush = [&](std::ofstream &ofs, bool force = false) {
-            auto head = m_head.load();
-            auto tail = m_tail.load();
+            size_t head,tail;
+            {
+              spin_lock_scope scope{m_spinlock};
+              head = m_head;
+              tail = m_tail;
 
-            if (head == tail) {
-              return;
+              if (head == tail) {
+                return;
+              }
+
+              auto used_space = m_head > m_tail
+                                    ? (m_head - m_tail)
+                                    : (buffer_size - m_tail + m_head);
+              if (!force && used_space < flush_treshhold) {
+                return;
+              }
+              m_tail = m_head;
             }
 
             if (head > tail) {
-              // std::cout << "HEAD > TAIL : tail: " << tail << " :: count " << head - tail << std::endl;
-
-              if(!force && head - tail < 5 * KByte)
-              {
-                return;
-              }
-
               ofs.write(reinterpret_cast<const char *>(m_buffer->data() + tail),
                         head - tail);
             } else {
-
-              if(!force && buffer_size - tail + head < 5 * KByte )
-              {
-                return;
-              }
-
-              // std::cout << "HEAD < TAIL : tail: " << tail << " :: count " << buffer_size - tail << std::endl;
               ofs.write(reinterpret_cast<const char *>(m_buffer->data() + tail),
                         buffer_size - tail);
-
-              // std::cout << "HEAD < TAIL : head: " << head << std::endl;
               ofs.write(reinterpret_cast<const char *>(m_buffer->data()), head);
             }
-            // std:: cout << "------- FLUSHING --------" << std::endl;
-            m_tail.store(head);
           };
 
           while (m_do_flushing) {
             execute_flush(ofs);
-            std::this_thread::sleep_for(std::chrono::microseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
           }
 
           execute_flush(ofs, true);
@@ -113,13 +149,11 @@ public:
   void set_do_flushing(const bool& value)
   {
     m_do_flushing = value;
-    std::cout << "--- END ---" << std::endl ;
     m_flushing_thread.join();
-    std::cout << "--- END ---" << std::endl ;
   }
 
-  template <typename... T> void store(sample_type type, T &&...values) {
-
+  template <typename... T> void store(sample_type type, T &&...values)
+  {
     auto arg_size = get_size(values...);
     size_t total_size = arg_size + sizeof(type) + sizeof(size_t);
     auto reserved_memory = reserve_memory_space(total_size);
@@ -151,39 +185,43 @@ private:
 
   void fragment_memory()
   {
-      std::lock_guard lock {m_fragment_mutex};
       auto data = m_buffer->data();
-      auto head = m_head.load();
 
-      *reinterpret_cast<sample_type *>(data + head) =
+      memset(data + m_head, 0xFFFF, buffer_size - m_head);
+
+      *reinterpret_cast<sample_type *>(data + m_head) =
           sample_type::fragmented_space;
 
-      size_t remining_bytes = buffer_size - head - minimal_fragmented_memory_size;
+      size_t remining_bytes = buffer_size - m_head - minimal_fragmented_memory_size;
 
-      *reinterpret_cast<size_t *>(data + head + sizeof(sample_type)) =
+      *reinterpret_cast<size_t *>(data + m_head + sizeof(sample_type)) =
           remining_bytes;
 
-      // std::cout << "fragmenting memory head at " << head << " / buffer size "
-      //           << buffer_size << std::endl;
-      m_head.store(0);
+      // std::cout << "fragmenting memory head at " << m_head << " / buffer size " << buffer_size << std::endl;
+      m_head = 0;
   }
 
-  uint8_t *reserve_memory_space(size_t len) {
 
-    // std::cout << m_head.load() << " + " << len << " + " << minimal_fragmented_memory_size << " > " << buffer_size << std::endl;
-
-    auto is_fragmentation_needed = (m_head.load() + len + minimal_fragmented_memory_size) > buffer_size;
-    if(is_fragmentation_needed)
+  uint8_t *reserve_memory_space(size_t len)
+  {
+    size_t size;
     {
-      fragment_memory();
+      spin_lock_scope scope{m_spinlock};
+
+      if ((m_head + len + minimal_fragmented_memory_size) > buffer_size) {
+        fragment_memory();
+      }
+      size = m_head;
+      m_head = m_head + len;
     }
-    auto size = m_head.fetch_add(len);
+
     auto result = m_buffer->data() + size;
     memset(result, 0, len);
     return result;
   };
 
-  template <typename... T> size_t get_size(T &...val) {
+  template <typename... T> size_t get_size(T &...val)
+  {
     auto get_size_impl = [&](auto val) {
       using Type = decltype(val);
       if constexpr (std::is_same_v<Type, const char *>) {
@@ -199,12 +237,12 @@ private:
   }
 
 private:
-  std::thread m_flushing_thread;
-  std::unique_ptr<BufferArray> m_buffer { std::make_unique<BufferArray>() };
-  std::atomic_size_t m_head{0};
-  std::atomic_size_t m_tail{0};
-  std::mutex m_fragment_mutex;
+  spin_lock m_spinlock;
   bool m_do_flushing { true };
+  std::thread m_flushing_thread;
+  size_t m_head{0};
+  size_t m_tail{0};
+  std::unique_ptr<buffer_array> m_buffer { std::make_unique<buffer_array>() };
 };
 
 class storage_parser
@@ -226,22 +264,27 @@ class storage_parser
     (process_arg(data_pos, args), ...);
   }
 public:
-  static void load(std::filesystem::path path) {
+  static size_t load(std::filesystem::path path) {
     std::ifstream ifs(path, std::ios::binary | std::ios::ate);
     if (!ifs) {
       std::cerr << "Error opening file for writing: " << path << std::endl;
-      return;
+      return 0;
     }
     std::ifstream::pos_type file_size = ifs.tellg();
     ifs.seekg(0, std::ios::beg);
 
     sample_type type;
     size_t sample_size;
-    size_t count = 0;
+    size_t total_count = 0;
+    size_t track_count = 0;
+    size_t process_count = 0;
+
     while(!ifs.eof())
     {
+
       ifs.read(reinterpret_cast<char*>(&type), sizeof(type));
       ifs.read(reinterpret_cast<char*>(&sample_size), sizeof(sample_size));
+
 
       if(sample_size == 0 || ifs.eof())
       {
@@ -251,6 +294,7 @@ public:
       std::vector<uint8_t> sample;
       // std::cout << "type:" << (int)type << "\n";
       // std::cout << "sample_size:" << sample_size << "\n";
+      // std::cout << "ifs.tellg():" << ifs.tellg() << "\n";
       sample.reserve(sample_size);
       ifs.read(reinterpret_cast<char*>(sample.data()), sample_size);
 
@@ -260,7 +304,10 @@ public:
         parse_data(sample.data(), data.track_name, data.node_id,
                    data.process_id, data.thread_id, data.extdata);
 
-        count++;
+        if(data.node_id == track_count++) {
+          total_count++;
+        }
+
         // std::cout << data.track_name << " " << data.node_id << " "
         //           << data.process_id << " " << data.thread_id << " "
         //           << data.extdata << std::endl;
@@ -271,6 +318,9 @@ public:
                    data.parent_process_id, data.process_id, data.init,
                    data.fini, data.start, data.end, data.command, data.env,
                    data.extdata);
+        if(data.node_id == process_count++) {
+          total_count++;
+        }
 
         // std::cout << data.guid << " " << data.node_id << " "
         //           << data.parent_process_id << " " << data.process_id << " "
@@ -284,7 +334,8 @@ public:
     }
 
     ifs.close();
-    std::cout << "Total tracks" << count << std::endl;
+    std::cout << "Total entries" << total_count << std::endl;
+    return total_count;
   }
 
 };
@@ -306,71 +357,66 @@ void store_process(storage& buffered_storage, std::string guid, size_t node_id, 
 
 int main() {
 
-  // rps_bencmark::init_from_env();
-
-  // auto thread1 = std::thread([]() {
-  //   auto node_id = 0;
-  //   auto process_id = 1;
-  //   auto thread_id = 2;
-  //   auto count = 0;
-  //   do {
-  //     rps_bencmark::start<benchmark::category::WriteTrack>();
-  //     cache::store_track("GPU 1", node_id++, process_id++, thread_id++, "{}");
-  //     rps_bencmark::end<benchmark::category::WriteTrack>();
-  //     count++;
-  //   }
-  //   while(count != 1000);
-  // });
-
-  // auto thread2 = std::thread([]() {
-  //   auto node_id = 0;
-
-  //   auto parent_process_id = 1;
-  //   auto process_id = 1;
-  //   auto init = 2;
-  //   auto fini = 2;
-  //   auto start = 2;
-  //   auto end = 2;
-  //   auto count = 0;
-  //   do {
-  //     rps_bencmark::start<benchmark::category::WriteProcess>();
-  //     cache::store_process("GUID", ++node_id, ++parent_process_id,++process_id,
-  //                          ++init, ++fini, ++start, ++end,
-  //                          "/my/command.exe", "{ BBBBBBBBBBBBBB }",
-  //                          "{AAAAAAAAAAAAAAAAAA}");
-  //                          count++;
-  //     rps_bencmark::end<benchmark::category::WriteProcess>();
-  //   }
-  //   while(count != 1000);
-  // });
-
-
-  // thread1.join();
-  // thread2.join();
-
-
-  // cache::storage empty;
-  // empty.load({"buffered_storage.bin"});
-  // empty.read();
-
-  // rps_bencmark::show_results();
+  rps_bencmark::init_from_env();
 
   cache::storage buffered_storage;
-  size_t node_id = 0;
-  auto process_id = 1;
-  auto thread_id = 2;
-  int count = 0;
-  while (count < 100000)
-  {
-    cache::store_track(buffered_storage, (std::string("GPU ") + std::to_string(count)).c_str(), node_id++, process_id++, thread_id++, "{}");
-    count++;
-    std::this_thread::sleep_for(std::chrono::microseconds(3));
-  }
+  auto thread1 = std::thread([&]() {
+    std::cout << "started thread 1" << std::endl;
+
+    auto node_id = 0;
+    auto process_id = 1;
+    auto thread_id = 2;
+    auto count = 0;
+    do {
+      rps_bencmark::start<benchmark::category::WriteTrack>();
+      cache::store_track(buffered_storage, "GPU 1", node_id++, process_id++, thread_id++, "{}");
+      rps_bencmark::end<benchmark::category::WriteTrack>();
+      count++;
+
+      // std::this_thread::sleep_for(std::chrono::microseconds(3));
+    }
+    while(count != 10000000);
+    std::cout << "ended thread 1" << std::endl;
+  });
+
+  auto thread2 = std::thread([&]() {
+    std::cout << "started thread 2" << std::endl;
+    auto node_id = 0;
+
+    auto parent_process_id = 1;
+    auto process_id = 1;
+    auto init = 2;
+    auto fini = 2;
+    auto start = 2;
+    auto end = 2;
+    auto count = 0;
+    do {
+      rps_bencmark::start<benchmark::category::WriteProcess>();
+      cache::store_process(buffered_storage, "GUID", node_id++, parent_process_id++,process_id++,
+                           init++, fini++, start++, end++,
+                           "/my/command.exe", "{ BBBBBBBBBBBBBB }",
+                           "{AAAAAAAAAAAAAAAAAA}");
+                           count++;
+      rps_bencmark::end<benchmark::category::WriteProcess>();
+      // std::this_thread::sleep_for(std::chrono::microseconds(4));
+    }
+    while(count != 10000000);
+    std::cout << "ended thread 2" << std::endl;
+  });
+
+  thread1.join();
+  thread2.join();
+
   buffered_storage.set_do_flushing(false);
 
   std::cout << "Writing done" << std::endl;
 
-  cache::storage_parser::load({ cache::filename });
+  auto read_count = cache::storage_parser::load({ cache::filename });
 
+  auto expected_count = 20000000;
+
+  std::cout << (read_count != expected_count ? "FAILED" : "SUCCESS") << std::endl;
+
+  rps_bencmark::show_results();
   return 0;
 }
